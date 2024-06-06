0675cfeba8ed8d063fe0b566f5cdfd959c0af2db
JOB-90 <136765423+JOB-90@users.noreply.github.com>
2024-06-05 22:40:10 -0300

    Joubert php (#3)
    * adicionado pasta
    
    * Feito exercícios PHP lógica de programação
    
    ---------
    
    Co-authored-by: Joubert Felipe <joubertfr90@gmail.bom>

--- PHP_CONCEITOS/exercicio-10.php
+++ PHP_CONCEITOS/exercicio-10.php
@@ -4,7 +4,23 @@ e calcula a soma de todos os números ímpares no intervalo [a, b] (inclusive).
 Certifique-se de que a seja menor ou igual a b. Se a for maior que b, solicite
 ao usuário que insira novamente os valores.*/
 
-$numero1= 4;
-$numero2= 18;
+echo "Informe o primeiro número: ";
+$numero1 = trim(fgets(STDIN));
+echo "Informe o segundo número: ";
+$numero2 = trim(fgets(STDIN));
 
-if ()
+$soma = 0;
+
+while ($numero1 > $numero2) {
+    echo "digite um numero maior que $numero1: ";
+    $numero2 = trim(fgets(STDIN));
+}
+    
+for($i= $numero1; $i <= $numero2 ; $i++) {
+    if($i%2=== 1){
+    $soma += $i;
+    $soma;
+    }
+}
+
+echo $soma;
