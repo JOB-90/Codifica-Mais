52256cf2cdcce8c7951fc7206b7efb13bb030d10
Joubert Felipe <joubertfr90@gmail.bom>
2024-06-16 14:27:24 -0300

    Feito exercício php-b7

---
+++ PHP-ARRAYS/php-b7.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$notasAlunos = [
+    $aluno1 = [8.5, 6.0, 7.8, 9.2, 5.5],
+    $aluno2 = [7.0, 8.0, 6.5, 7.5, 8.5],
+    $aluno3 = [6.5, 7.5, 4.5, 5.5, 7.0],
+    $aluno4 = [5.0, 4.6, 7.8, 9.0, 6.0] 
+    ];
+
+    function ($notas){
+        for ($i = 0; $i < 5; $i++){
+            $notasAlunos[$i] + $notasAlunos[$i];
+            return $notas;
+        }
+    }
+
+    echo $notas;
