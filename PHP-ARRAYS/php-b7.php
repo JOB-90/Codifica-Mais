3952224b55d3bb91b4c9c36388be99bdcd8ef048
Joubert Felipe <joubertfr90@gmail.bom>
2024-06-17 20:25:34 -0300

    Feito exercício php-b7

--- PHP-ARRAYS/php-b7.php
+++ PHP-ARRAYS/php-b7.php
@@ -1,5 +1,19 @@
 <?php
 
+function media($notasAluno){
+    $result = 0;
+    for ($i = 0; $i < 5; $i++) {
+    
+        $result += $notasAluno[$i];
+
+    }
+
+    return $result / 5;
+}
+
+$totalApro = 0;
+$totalRep = 0;
+
 
 $notasAlunos = [
     $aluno1 = [8.5, 6.0, 7.8, 9.2, 5.5],
@@ -8,11 +22,20 @@ $notasAlunos = [
     $aluno4 = [5.0, 4.6, 7.8, 9.0, 6.0] 
     ];
 
-    function ($notas){
-        for ($i = 0; $i < 5; $i++){
-            $notasAlunos[$i] + $notasAlunos[$i];
-            return $notas;
-        }
-    }
+for ($i = 0; $i < 4; $i++){
+    $resultAlu = media($notasAlunos[$i]);
 
-    echo $notas;
+
+    if ($resultAlu >= 7){
+       $totalApro = $totalApro + 1;
+        echo $i . " - Sua média é: " . $resultAlu . ". você esta aprovado.\n";
+     }
+    else {
+        $totalRep = $totalRep + 1;
+        echo $i . " - Sua média é: " . $resultAlu . ". você esta reprovado.\n";
+    }
+}
+
+echo "----------------------------------\n";
+echo "total de alunos aprovados: " . $totalApro . "\n";
+echo "Total de alunos reprovados: " . $totalRep;
