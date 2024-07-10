061967ecfbb346ffed0e9ec4c68b953cb722536f
Joubert Felipe <joubertfr90@gmail.bom>
2024-07-09 21:15:49 -0300

    jf-cod-php-b11

--- PHP-ARRAYS/php-b11.php
+++ PHP-ARRAYS/php-b11.php
@@ -1,25 +1,96 @@
 <?php
 
-$estoque = [];
 
-$estoque[] = [
-    'codigo' => 1,
-    'nome' => 'Camisa Polo',
-    'tamanho' => 'P',
-    'cor' => 'Branca',
-    'quantidade' => 1
+$estoque[] = 
+    [
+    'codigo' => 3,
+    'nome' => 'Camisa Regata',
+    'tamanho' => 'M',
+    'cor' => 'Preta',
+    'quantidade' => 0
     ];
+    $estoque[] = 
+    [
+    'codigo' => 5,
+    'nome' => 'Chinelo',
+    'tamanho' => 'p',
+    'cor' => 'Marron',
+    'quantidade' => 0
+    ];
+    $estoque[] = 
+    [
+    'codigo' => 8,
+    'nome' => 'Bota',
+    'tamanho' => 'g',
+    'cor' => 'branca',
+    'quantidade' => 95
+    ];
+
+
+
+
+
+
+
+
+
+
+
 // Implemente aqui o código
 function listarEstoque($estoque){
     if (empty($estoque)) {
         echo 'estoque vasio!\n';}
     foreach($estoque as $produto){
-        echo $produto['cor'];}
+        echo "{$produto['codigo']}\n";
+        echo "{$produto['nome']}\n";
+        echo "{$produto['tamanho']}\n";
+        echo "{$produto['cor']}\n";
+        echo "{$produto['quantidade']}\n";
+        
+    }
 }
+
+
 function adicionarProduto(&$estoque, $codigo, $nome, $tamanho, $cor, $quantidade)
 {
+   // sort ()
+       $estoque [] = [
+        'codigo' => $codigo,
+        'nome' => $nome,
+        'tamanho' => $tamanho,
+        'cor' => $cor,
+        'quantidade' => $quantidade
+        ];
 
 }
+
+
+function venderProduto (&$estoque, $codigo, $quantidade){
+ // print_r($estoque);
+    for ($i = 0; $i < count($estoque); $i++){
+        //print_r($estoque[$i]);
+
+        if ($estoque[$i]["codigo"]==$codigo){
+            $estoque[$i]["quantidade"] -= $quantidade;
+        }
+        
+    }    
+
+}
+
+
+function verificar (&$estoque){
+    for ($i = 0; $i < count($estoque); $i++){
+        //print_r($estoque[$i]);
+
+        if ($estoque[$i]["quantidade"]== 0){
+            echo "este produto esta esgotado, precia repor.";
+            var_dump($estoque[$i]);
+        }
+    }
+}
+
+
 
 
 // Função para exibir o menu e obter a escolha do usuário
@@ -52,15 +123,23 @@ while (true) {
             break;
         case 2:
             echo "Vender um produto\n";
+            $codigo = readline("Digite código do produto: ");
+            $quantidade = readline("Digite a Quantidade: ");
+            venderProduto($estoque,$codigo,$quantidade);
             // Implemente aqui o código para a opção 2
+
+
             break;
         case 3:
             echo "Verificar Estoque\n";
+             verificar ($estoque);
             // Implemente aqui o código para a opção 3
+
+
             break;
         case 4:
             echo "Listar o Estoque\n";
-            listarEstoque($estoque);     
+            listarEstoque($estoque);
             // Implemente aqui o código para a opção 4
             break;
         case 5:
